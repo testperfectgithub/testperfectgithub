6bacdbea59660e85e01424dc3e8981527f4d256d
Gonthurothulu Balaram <159694992+testperfectgithub@users.noreply.github.com>
2024-03-15 20:44:16 +0530

    Delete Assignment directory

--- Assignment/PrintMillitersAndLiters.java
+++
@@ -1,8 +0,0 @@
-package Assignment;
-
-public class PrintMillitersAndLiters {
-public static void main(String[] args) {
-	double Ml=2500.5;double Li=Ml/1000;
-	System.out.println(String.format("Millileters(%s)=>Liters((%.3f)", Ml,Li));
-}
-}
